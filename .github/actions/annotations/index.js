2e171fdd39f95e9fb08ee26cffcbe7e018b8b644
Teo Koon Peng <koonpeng@openrobotics.org>
2021-09-02 11:31:52 +0800

    push
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- .github/actions/annotations/index.js
+++ .github/actions/annotations/index.js
@@ -1,10 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-core.error('an error message');
-core.warning('a warning message');
-core.info('an info message');
-core.notice('a notice message');
+// core.error('an error message');
+// core.warning('a warning message');
+// core.info('an info message');
+// core.notice('a notice message');
 
-const octo = github.getOctokit(github.context.token);
+const octo = github.getOctokit(core.getInput('ghToken'));
 octo.rest.checks.create({ name: 'test check', head_sha: github.context.sha });
