66f0df21fbfbd7ba601ee717fc3c0d76a54e4648
Teo Koon Peng <koonpeng@openrobotics.org>
2021-09-02 11:11:15 +0800

    push
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- .github/actions/annotations/index.js
+++ .github/actions/annotations/index.js
@@ -3,4 +3,4 @@ const core = require('@actions/core');
 core.error('an error message');
 core.warning('a warning message');
 core.info('an info message');
-core.info('https://google.com');
+core.warning('https://google.com');
